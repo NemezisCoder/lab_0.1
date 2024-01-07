bfbd064f8807451fb6cd9675bb34fcc727bb1173
Nemezis <ila853985@gmail.com>
2024-01-07 18:09:45 +0300

    Change in secondary-branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // Выводит "Привет, Мир!" в консоль
-        System.out.println("Привет, Мир!");
+        System.out.println("Привет, Мыр!");
     }
 }
